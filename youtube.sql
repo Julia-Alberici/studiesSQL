9750b32e97e7851fd50c48fefdd5c3fdf3c5a3c8
Julia-Alberici <julia.jpa0@gmail.com>
2022-07-04 17:56:15 -0300

    add value to table videos

--- youtube.sql
+++ youtube.sql
@@ -11,4 +11,10 @@ CREATE TABLE videos (
 	id_video INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome_video VARCHAR(100) NOT NULL,
     autor_video VARCHAR(30) NOT NULL
-)
+);
+
+INSERT INTO videos (nome_video, autor_video) VALUES ('Login com React', 'React');
+INSERT INTO videos (nome_video, autor_video) VALUES ('Components com React', 'React');
+INSERT INTO videos (nome_video, autor_video) VALUES ('Listas com PHP', 'PHP');
+INSERT INTO videos (nome_video, autor_video) VALUES ('Funções com PHP', 'PHP');
+INSERT INTO videos (nome_video, autor_video) VALUES ('Páginas com HTML', 'HTML');
