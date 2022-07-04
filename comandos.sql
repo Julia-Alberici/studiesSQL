a0a549484f3f45b3d4794e802fef5e1e6bc346dd
Julia-Alberici <julia.jpa0@gmail.com>
2022-07-04 15:30:23 -0300

    Aprendendo a editar dados

--- comandos.sql
+++ comandos.sql
@@ -7,3 +7,5 @@ CREATE TABLE pessoas (
 INSERT INTO pessoas (nome, nascimento) VALUES ('Julia', '2001-12-19')
 INSERT INTO pessoas (nome, nascimento) VALUES ('Isa', '2002-02-21')
 INSERT INTO pessoas (nome, nascimento) VALUES ('Fer', '1977-04-04')
+
+UPDATE pessoas SET nome= 'Fernanda' WHERE id=3; 
