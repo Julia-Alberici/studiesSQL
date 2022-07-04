03a0fbccd88b8402f5fd031355a721a6c4f421f0
Julia-Alberici <julia.jpa0@gmail.com>
2022-07-04 16:02:26 -0300

    setando coluna genero

--- comandos.sql
+++ comandos.sql
@@ -17,3 +17,8 @@ DELETE FROM pessoas WHERE id=4;
 SELECT * FROM pessoas ORDER BY nome;
 
 ALTER TABLE pessoas ADD genero VARCHAR(1) NOT NULL AFTER nascimento;
+
+UPDATE pessoa SET genero='F' WHERE id=1;
+UPDATE pessoa SET genero='F' WHERE id=2;
+UPDATE pessoa SET genero='F' WHERE id=3;
+UPDATE pessoa SET genero='F' WHERE id=5;
