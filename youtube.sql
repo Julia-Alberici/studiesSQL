38354174a818c2b0732aa7a6a3a9514c00168fc9
Julia-Alberici <julia.jpa0@gmail.com>
2022-07-04 17:48:25 -0300

    Add values

--- youtube.sql
+++ youtube.sql
@@ -2,3 +2,7 @@ CREATE TABLE canais (
     id_canal INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome_canal VARCHAR(30) NOT NULL
 )
+
+INSERT INTO canais (nome_canal) VALUES ('React');
+INSERT INTO canais (nome_canal) VALUES ('PHP');
+INSERT INTO canais (nome_canal) VALUES ('CSS');
