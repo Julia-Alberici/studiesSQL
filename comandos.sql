48092307d4fa652cffb34890d11c0df2e387fc5c
Julia-Alberici <julia.jpa0@gmail.com>
2022-07-04 15:38:58 -0300

    Inserindo mais dados

--- comandos.sql
+++ comandos.sql
@@ -4,8 +4,10 @@ CREATE TABLE pessoas (
     nascimento DATE
 )
 
-INSERT INTO pessoas (nome, nascimento) VALUES ('Julia', '2001-12-19')
-INSERT INTO pessoas (nome, nascimento) VALUES ('Isa', '2002-02-21')
-INSERT INTO pessoas (nome, nascimento) VALUES ('Fer', '1977-04-04')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Julia', '2001-12-19');
+INSERT INTO pessoas (nome, nascimento) VALUES ('Isa', '2002-02-21');
+INSERT INTO pessoas (nome, nascimento) VALUES ('Fer', '1977-04-04');
+INSERT INTO pessoa (nome, nascimento) VALUES ('Luan', '1992-10-01');
+INSERT INTO pessoa (nome, nascimento) VALUES ('Aurora', '2019-05-10');
 
 UPDATE pessoas SET nome= 'Fernanda' WHERE id=3; 
