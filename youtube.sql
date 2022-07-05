047ff35e1c232cf0ae3b120d188be88048662400
Julia-Alberici <julia.jpa0@gmail.com>
2022-07-05 17:19:01 -0300

    add column author as a foreign key and setting values

--- youtube.sql
+++ youtube.sql
@@ -26,3 +26,13 @@ INSERT INTO author (name, born, phone) VALUES ('João', '1995-05-20', 35163874);
 INSERT INTO author (name, born, phone) VALUES ('Flávia', '1985-08-01', 30145366);
 
 ALTER TABLE videos DROP COLUMN author;
+ALTER TABLE videos ADD author INT NOT NULL AFTER id_video;
+UPDATE videos SET author=1 WHERE id_video=1;
+UPDATE videos SET author=1 WHERE id_video=2;
+UPDATE videos SET author=1 WHERE id_video=3;
+UPDATE videos SET author=2 WHERE id_video=4;
+UPDATE videos SET author=3 WHERE id_video=5;
+
+/*Rename column author from table videos to fk_author and
+turn into a foreign key to id_author from table author by phpMyAdmin */
+
